978e762c23b0b69f2d07103066e9afddbf9fd900
mbrayton27 <56379807+mbrayton27@users.noreply.github.com>
2021-02-11 14:33:54 -0800

    Add files via upload

--- test_cube.py
+++ test_cube.py
@@ -7,8 +7,8 @@ class TestCube(unittest.TestCase):
         self.assertEqual(result, 64)
 
     def test_cube(self):
-        result = cube.vol(4)
-        self.assertEqual(result, 64)
+        result = cube.vol(1.5)
+        self.assertEqual(result, 3.375)
 
     def test_cube3(self):
         result = cube.vol(-1)
